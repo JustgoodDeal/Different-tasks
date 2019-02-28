e520d2478769935367a43c716724af6a70a06d90
JustgoodDeal <stostoraz@gmail.com>
2019-02-28 13:45:38 +0300

    q

--- HomeWork/Class 6/Task1.py
+++ HomeWork/Class 6/Task1.py
@@ -1,5 +1,7 @@
 # Написать программу нахождения простых чисел, используя решето Эратосфена. Для каждой из пользовательских функций написать функцию-тест.
 
+# -*- coding: utf-8 -*-
+import argparse
 def prostie_chisla(n):
     nums = []
     for i in range(2,n+1):
@@ -13,7 +15,12 @@ def Test_prostie_chisla():
     assert prostie_chisla(7) == [2,3,5,7], 'Тест провален'
     assert prostie_chisla(199) == [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89,
     97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199], 'Тест провален'    
-n = int(input())
+
+parser = argparse.ArgumentParser()
+parser.add_argument("a", type=int,nargs='?', default=8)
+args = parser.parse_args()
+
+n = args.a 
 easy = prostie_chisla (n)
 print(' '.join([str(i) for i in easy]))
 Test_prostie_chisla()
